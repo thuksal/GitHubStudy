0539977742d5514fc0900eb8dc165c9fdaeda3e2
leejaesung <blackturtle2@gmail.com>
2017-07-10 20:13:06 +0900

    2nd commit, Add MainViewController

--- GithubTestProject/GithubTestProject/MainViewController.swift
+++ GithubTestProject/GithubTestProject/MainViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+    
+    @IBOutlet var textFieldMain:UITextField?
+    @IBOutlet var buttonMain:UIButton?
+    @IBOutlet var labelMain:UILabel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonMainAction(_ sender:UIButton) {
+        labelMain?.text = textFieldMain?.text
     }
 
 
